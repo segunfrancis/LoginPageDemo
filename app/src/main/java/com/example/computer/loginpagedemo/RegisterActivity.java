15d9a8ab714e7c7d4ad3aeba9b2d988e24494a3a
segunfrancis <francis4dblues@gmail.com>
2019-09-24 08:22:15 +0100

    Login Page Demo: Fix login button

--- app/src/main/java/com/example/computer/loginpagedemo/RegisterActivity.java
+++ app/src/main/java/com/example/computer/loginpagedemo/RegisterActivity.java
@@ -4,14 +4,16 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private SharedPreferences mPreferences;
     private EditText mEmail;
-    private EditText mPassword;
+    private EditText mPassword, mConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +23,28 @@ public class RegisterActivity extends AppCompatActivity {
         mPreferences = getSharedPreferences(getPackageName() + ".my_pref", MODE_PRIVATE);
 
         mEmail = findViewById(R.id.editText_email);
-        mPassword = findViewById(R.id.editText_confirm_password);
+        mPassword = findViewById(R.id.editText_password);
+        mConfirmPassword = findViewById(R.id.editText_confirm_password);
     }
 
     public void register(View view) {
         String userEmail = mEmail.getText().toString().trim();
-        String userPassword = mPassword.getText().toString();
+        String userPassword = mConfirmPassword.getText().toString();
 
-        SharedPreferences.Editor editor = mPreferences.edit();
-        editor.putString(userEmail + MainActivity.USERNAME, userEmail);
-        editor.putString(userEmail + MainActivity.PASSWORD, userPassword);
-        editor.apply();
+        if (TextUtils.equals(userPassword, mPassword.getText().toString())) {
 
+            SharedPreferences.Editor editor = mPreferences.edit();
+            editor.putString(userEmail + MainActivity.USERNAME, userEmail);
+            editor.putString(userEmail + MainActivity.PASSWORD, userPassword);
+            editor.apply();
+
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void redirectLogin(View view) {
         startActivity(new Intent(this, MainActivity.class));
     }
 }
